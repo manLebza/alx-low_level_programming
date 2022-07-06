9540f81c55f5a1d3ebcd4ff9096b6cd30748354f
manLebza <manlebza.mokoena1@gmail.com>
2022-07-06 14:25:03 -0700

    24 hours fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,20 +21,20 @@ void jack_bauer(void)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
-			}
+ 			}
 			if (j < 10)
 			{
 				_putchar(':');
 				_putchar('0');
 				_putchar(j + 10);
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
-
+			_putchar('\n');
 		}
 	}
 }
