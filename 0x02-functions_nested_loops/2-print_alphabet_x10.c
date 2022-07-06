e1d1675de99f9422d2025305bd7e320b9bc0dcfc
manLebza <manlebza.mokoena1@gmail.com>
2022-07-06 05:46:05 -0700

    x10 alphabet fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,12 @@
 /**
  * print_alphabet_x10 -> prints the alphabet x10
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int j;
-	int y;
+	int x;
 
-	for (y = 0; y < 10 ; y++)
+	for (x = 0; x < 10 ; x++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
