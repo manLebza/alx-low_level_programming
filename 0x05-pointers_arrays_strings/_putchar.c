e39c3be21318eaa62d22c2f64fca3f56b41f46ca
manLebza <manlebza.mokoena1@gmail.com>
2022-07-11 16:12:13 -0700

    main protect

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
-
 /**
  * _putchar -writes the character c to stdout
  * @c: The character printed
  * Return:on success 1.
- * on error,-1 is returned
+ * on error,-1 is returned,and errno is set appropiately
  */
 
 int _putchar(char c)
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +11,4 @@ void put2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
 #endif
