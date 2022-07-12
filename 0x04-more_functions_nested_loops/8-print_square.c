6366d1fddabaa43fc55fc5787e61059e7d2ae8f6
manLebza <manlebza.mokoena1@gmail.com>
2022-07-12 08:56:22 -0700

    fix till 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square -> prints a square
+ * @size: prints the size of the square
+ * Return: always 0.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int k;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				_putchar (35);
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_puchar ('\n');
+	}
+}
