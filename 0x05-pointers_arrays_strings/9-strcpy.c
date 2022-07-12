ee0daed60e011fae1ce95bdc8af90e2db955f957
manLebza <manlebza.mokoena1@gmail.com>
2022-07-11 17:44:12 -0700

    strcyp

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,21 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- *
- *
+ * _strcyp -> copies the variable source
+ * @dest: This is the destination
+ *  @src: This is the copied source
+ *   Return: return copy
  */
+char *_strcyp(char *dest, char *src)
+{
+	char *start = dest;
 
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
