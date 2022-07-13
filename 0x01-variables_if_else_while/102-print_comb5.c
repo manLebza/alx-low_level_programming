5d6385570910fe7c004b5da1c94cc8a108ffe484
manLebza <manlebza.mokoena1@gmail.com>
2022-07-13 14:43:20 -0700

    all addedin

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -> entry point
+ * Return: Always returns 0.
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; j <= 90; j++)
+	{
+		for (i = j; i <= 99; i++)
+		{
+			if (i != j)
+			{
+				putchar (j / 10 + 48);
+				putchar (j % 10 + 48);
+				putchar (' ');
+				putchar (i / 10 + 48);
+				putchar (i % 10 + 48);
+
+				if (j * 100 + i != 9899)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
