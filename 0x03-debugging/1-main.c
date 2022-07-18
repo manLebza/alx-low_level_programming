c0c98f843e55544687e6da65bae76f34d6a01b22
manLebza <manlebza.mokoena1@gmail.com>
2022-07-18 07:28:40 -0700

    betty fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,8 +2,8 @@
 
 /**
  * main - causes an infinite loop
- * @while - this loop is commented out in oder to get the required outcome
- * @printf - this statement is declaration of the break in the loop.
+ * while - this loop is commented out in oder to get the required outcome
+ * printf - this statement is declaration of the break in the loop.
  * Return: 0
  */
 
@@ -16,12 +16,13 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /**
+ * printf -> loop break declaration
 * while (i < 10)
 * {
 *	putchar(i);
 * }
 */
-
+/*printf -> break statement in infinite loop*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 /**
- * main - this is the main method
  * largest_number -> returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
