a91ec416a3a7f56b15ca55888a61401adc76802f
manLebza <manlebza.mokoena1@gmail.com>
2022-07-18 07:43:08 -0700

    betty fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * while - this loop is commented out in oder to get the required outcome
  * printf - this statement is declaration of the break in the loop.
+ * @\\o/\n - break statement precceded by a new  line
  * Return: 0
  */
 
@@ -17,6 +18,7 @@ i = 0;
 
 /**
  * printf -> loop break declaration
+ * "\\o/\n" - this is a break statement precceded by a new line
 * while (i < 10)
 * {
 *	putchar(i);
