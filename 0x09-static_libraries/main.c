01171ba0c2b398e410c22ff9977cf030099c963a
manLebza <manlebza.mokoena1@gmail.com>
2022-07-23 08:29:50 -0700

    hot fix 1

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
-int Main(void)
+/**
+ * main -> entry point of program
+ * Return: Always 0.
+ */
+
+int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t-Kevin Mitnick");
+	_puts("\"At the end of the day,
+			my goal was to be the best hacker\"\n\t-Kevin Mitnick");
 	return (0);
 }
