6f8e66200d89cfcd49e3830d09125c533ca5d44d
manLebza <manlebza.mokoena1@gmail.com>
2022-07-28 17:00:07 -0700

    first malloc commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,6 +4,7 @@
 
 /**
  * string_nconcat -> string concatination implementation
+ * @n: unsigned int
  * @s1: param 1
  * @s2: param 2
  * Return: concated string
@@ -11,4 +12,26 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int
+	unsigned int len = n, index;
+	char *concat;
+
+	if (s1 == NUL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * len + 1);
+	if (concat == NULL)
+		return (NULL);
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+	return (concat);
+}
