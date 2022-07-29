602677078001086e93efa58cfa039fcd30d16afb
manLebza <manlebza.mokoena1@gmail.com>
2022-07-28 19:41:35 -0700

    all hot fixxed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,31 +7,36 @@
  * @n: unsigned int
  * @s1: param 1
  * @s2: param 2
+ * @n: num of bytes to concat from string 2.
  * Return: concated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = n, index;
+	unsigned int len, num, index, x;
 	char *concat;
 
-	if (s1 == NUL)
+	num = n;
+
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (index = 0; s1[index]; index++)
-		len++;
+	if (num < 0)
+		return (NULL);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
 
-	concat = malloc(sizeof(char) * len + 1);
+	len = _strlen(s1) + num + 1;
+	concat = malloc(sizeof(*concat) * len + 1);
 	if (concat == NULL)
 		return (NULL);
-	len = 0;
 
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
+	for (index = 0; s1[index] != '\0'; index++)
+		concat[index] = s1[index];
+	for (x = 0; x < num; x++)
+		concat[index + x] = s2[x];
+	concat[index + x] = '\0';
 
-	concat[len] = '\0';
 	return (concat);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -7,4 +11,10 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _puchar(char c);
+int len(char *str);
+int find_len(char *str);
+char *create_xarray(int size);
+chariterate_zeroes(char *str);
+void get_prod(char *pod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 #endif
