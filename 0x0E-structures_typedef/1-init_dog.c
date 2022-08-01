fb13a7c136f469d1f7dfddd0556ea070d89bec6c
manLebza <manlebza.mokoena1@gmail.com>
2022-08-01 10:14:16 -0700

    first try

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ *
+ *
+ */
+
+void init_dog(struct dog *d,
+		char *name, float age, char *owner)
+{
+	struct dog
+	{
+		char *name;
+		char *owner;
+		float age;
+	}
+}
+
+int main()
+{
+	struct dog p = {;
+	char  *p = &dog;
+
+	return(dog);
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This module focuses on the data type "struct"
+in C programming and how it is used in the language.
