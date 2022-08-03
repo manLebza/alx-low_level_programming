919cb86b09fcfdde01b62cca793aa8ebdac7e1aa
manLebza <manlebza.mokoena1@gmail.com>
2022-08-02 18:57:08 -0700

    all set

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,25 +1,24 @@
 #include "dog.h"
 
 /**
- *
- *
+ * init_dog -> initialises a var. of type struct dog
+ * @d: pointer to struct
+ * @name: 1st member
+ * @age:2nd member
+ * @owner: 3rd member
+ * struct dog -> struct member type
+ * Retrun: void
  */
 
 void init_dog(struct dog *d,
 		char *name, float age, char *owner)
 {
-	struct dog
-	{
-		char *name;
-		char *owner;
-		float age;
-	}
-}
+	if (d == 0)
+		return;
 
-int main()
-{
-	struct dog p;
-	char  *p = &dog;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 
-	return(p);
+	return (0);
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * new_dog -> creates new dog obj.
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ * Return: NULL on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, leng, lenh,
+	    struct dog *n_dog = NULL;
+
+	leng =o;
+	while (name[leng] != '\0')
