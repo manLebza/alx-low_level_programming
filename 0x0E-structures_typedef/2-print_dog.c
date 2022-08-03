663dcf25abcfd7185dc6d48cb32d0765df27f7f3
manLebza <manlebza.mokoena1@gmail.com>
2022-08-02 20:02:47 -0700

    all hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %S\n", d->name);
 
-		printf("Age %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
