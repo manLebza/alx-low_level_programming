3ce7decc7df9dfb6bda2f0c7171ca5261a36882e
manLebza <manlebza.mokoena1@gmail.com>
2022-08-11 16:51:25 -0700

    all linked hot fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,7 +11,7 @@ size_t list_len(const list_t *h)
 {
 	int elemnt;
 
-	for (elemnt = 0; h != '\0'; elemnt++)
+	for (elemnt = 0; h; elemnt++)
 		h = h->next;
 	return (elemnt);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 
+
+int _strlen(const char *s)
+{
+	int x;
+
+	for (x = 0; s[x]; x++)
+		;
+	return (x);
+}
 /**
  * add_node -> adds new node in begining of list
  * @head: head of node
@@ -10,21 +19,29 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *n_node;
-	int  n_elemnt = 0;
+	char *content;
+	int  n_elemnt = 0, x;
 
-	n_node = malloc(sizeof(list_t));
-
-	if (n_node == NULL)
+	if (str == NULL || head == NULL)
 		return (NULL);
+	n_elemnt = _strlen(str);
+	new = *head;
+	content = malloc((n_elemnt + 1) * sizeof(char));
 
-	n_node->str = strdup(str);
-	while (str[n_elemnt])
+	if (content == NULL)
+		return (NULL);
+	for (x = 0; str[x]; x++)
+		content[x] = str[x];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		n_elemnt++;
+		free(content);
+		return (NULL);
 	}
-	n_node->len = n_elemnt;
-	n_node->next = *head;
-	*head = n_node;
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
 
-	return (*head);
+	return (new);
 }
