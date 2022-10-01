032bb09b43ce9fbd1cbcb71190b9b3bf00c3d90d
manLebza <manlebza.mokoena1@gmail.com>
2022-09-30 17:36:45 -0700

    hot fix2

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -13,16 +13,10 @@ int sum_dlistint(dlistint_t *head)
 
 	sum = 0;
 
-	if (head != NULL)
+	while (head != NULL)
 	{
-		while (head->prev != NULL)
-			head = head->prev;
-
-		while (head != NULL)
-		{
-			sum += head->n;
-			head = head->next;
-		}
+		sum += head->n;
+		head = head->next;
 	}
 
 	return (sum)
