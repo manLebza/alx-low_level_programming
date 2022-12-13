4ded76a755a56709992288333d8b49c17e940971
manLebza <manlebza.mokoena1@gmail.com>
2022-12-13 23:57:22 +0000

    all makefiles in

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module contains the island_parameter function """
+
+
+def island_perimeter(grid):
+    """ Function returns the parimeter of the island described """
+
+    perimeter = 0
+
+    nrows = len(grid)
+
+    if grid ![]:
+        ncolumns = len(grid[0])
+
+    for a in range(nrows):
+        for b in range(ncolumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == nrows or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,3 @@
+In the module we learned how to impilement makefiles in C programming
+using C programming language implemented in Linux 20.04 Ubuntu LTS,
+compiled using GCC(gnu89).
