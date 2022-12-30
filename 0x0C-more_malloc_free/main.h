0d014a110ecec68853a5ece37ee7133b467f2aa8
manLebza <manlebza.mokoena1@gmail.com>
2022-12-30 21:09:46 +0000

    more malloc fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -12,10 +10,4 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _puchar(char c);
 
-int len(char *str);
-int find_len(char *str);
-char *create_xarray(int size);
-chariterate_zeroes(char *str);
-void get_prod(char *pod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
 #endif
